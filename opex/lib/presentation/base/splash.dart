646d8e3bd29cee6c3715374c312e53b65c47f231
MACBOOK <nishad@rgcdynamics.com>
2023-09-02 09:06:21 +0530

    latest designs and splash

--- opex/lib/presentation/base/splash.dart
+++ opex/lib/presentation/base/splash.dart
@@ -1,4 +1,3 @@
-
 import 'package:cluster/presentation/authentication/authentication.dart';
 import 'package:cluster/presentation/base/onboarding.dart';
 import 'package:flutter/material.dart';
@@ -6,12 +5,9 @@ import 'package:lottie/lottie.dart';
 import 'dart:async';
 import 'dashboard.dart';
 
-
-
 class SplashScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return SplashScreenState();
   }
 }
@@ -20,44 +16,40 @@ class SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3),
-            ()=>
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder:
-                    (context) =>
-                // HomePage()
-                    authentication.isAuthenticated?const DashBoard() :const OnBoarding()
-
-                )
-            )
-    );
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    // HomePage()
+                    authentication.isAuthenticated
+                        ? const DashBoard()
+                        : const OnBoarding())));
   }
 
   @override
   Widget build(BuildContext context) {
-    var h=MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.width;
     debugPrint("${authentication.authenticatedUser.token}customer_CODE");
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: PageView(
-            children:[
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                     height: h/2,
-                      alignment: Alignment.center,
-                      child: Lottie.asset('asset/Logo.json'),
-                      // Image.asset('asset/Logo'),
-                      //child: SvgPicture.string(IconConstants().SplashIcon),
-                    )
-                  ]),
-            ]),
+        child: PageView(children: [
+          Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: h / 2,
+                  alignment: Alignment.center,
+                  child: Lottie.asset('asset/Logo.json'),
+                  // Image.asset('asset/Logo'),
+                  //child: SvgPicture.string(IconConstants().SplashIcon),
+                )
+              ]),
+        ]),
       ),
-
     );
   }
 }
-
